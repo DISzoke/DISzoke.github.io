327dfe929c89bede45dd6941ef2410af9197d1bf
DISzoke <155892467+DISzoke@users.noreply.github.com>
2024-03-13 02:20:59 +0200

    Initial Commit

---
+++ app-files/data.js
@@ -0,0 +1,557 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.6251937077880925,
+        "pitch": 0.1161137983411642,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3186550696195134,
+          "pitch": 0.10798765665279575,
+          "rotation": 0,
+          "target": "1-entrance-2"
+        },
+        {
+          "yaw": -1.4518283678538086,
+          "pitch": -0.039173069108390024,
+          "rotation": 0,
+          "target": "2-mix-n-match"
+        },
+        {
+          "yaw": -0.9697768787226355,
+          "pitch": -0.14735780851918534,
+          "rotation": 0,
+          "target": "3-restaurant"
+        },
+        {
+          "yaw": -0.4417017999457542,
+          "pitch": -0.1430985813061305,
+          "rotation": 0,
+          "target": "4-playground-tree"
+        },
+        {
+          "yaw": -0.04219423230622965,
+          "pitch": -0.07016592929345578,
+          "rotation": 0,
+          "target": "8-pots"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrance-2",
+      "name": "Entrance 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6711805990965853,
+          "pitch": 0.07991586677655782,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": -0.4471856587543339,
+          "pitch": -0.04786517261792511,
+          "rotation": 0,
+          "target": "8-pots"
+        },
+        {
+          "yaw": -0.8461179798435374,
+          "pitch": -0.0671142966774525,
+          "rotation": 0,
+          "target": "7-playpark-entrance"
+        },
+        {
+          "yaw": -1.3881625820549104,
+          "pitch": -0.03253927188492867,
+          "rotation": 0,
+          "target": "2-mix-n-match"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-mix-n-match",
+      "name": "Mix n Match",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1160826787852205,
+          "pitch": -0.09096296294488937,
+          "rotation": 0,
+          "target": "3-restaurant"
+        },
+        {
+          "yaw": 0.8367411250463732,
+          "pitch": -0.0922946644752276,
+          "rotation": 0,
+          "target": "7-playpark-entrance"
+        },
+        {
+          "yaw": 1.2217826741324895,
+          "pitch": -0.06582001518588854,
+          "rotation": 0,
+          "target": "8-pots"
+        },
+        {
+          "yaw": 2.273741972544798,
+          "pitch": -0.021133919068605067,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 2.590544190591758,
+          "pitch": -0.005106156829398856,
+          "rotation": 0,
+          "target": "1-entrance-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-restaurant",
+      "name": "Restaurant",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4086384006800152,
+          "pitch": 0.03782572688359487,
+          "rotation": 0,
+          "target": "4-playground-tree"
+        },
+        {
+          "yaw": 1.2915177923420842,
+          "pitch": 0.06369153244500936,
+          "rotation": 0,
+          "target": "7-playpark-entrance"
+        },
+        {
+          "yaw": -3.065677002739104,
+          "pitch": 0.12697062122227187,
+          "rotation": 0,
+          "target": "2-mix-n-match"
+        },
+        {
+          "yaw": 2.540060849505686,
+          "pitch": 0.04708871070235965,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 2.8385845611015466,
+          "pitch": 0.04488640515643638,
+          "rotation": 0,
+          "target": "1-entrance-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-playground-tree",
+      "name": "PlayGround Tree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11437960187023322,
+          "pitch": 0.08756800507108764,
+          "rotation": 0,
+          "target": "3-restaurant"
+        },
+        {
+          "yaw": -2.4845715854538053,
+          "pitch": 0.23415094745906728,
+          "rotation": 0,
+          "target": "6-bridge-2"
+        },
+        {
+          "yaw": 3.0896548568726203,
+          "pitch": 0.19648534702822573,
+          "rotation": 0,
+          "target": "6-bridge-2"
+        },
+        {
+          "yaw": -1.4295449212228597,
+          "pitch": 0.1935126951382049,
+          "rotation": 0,
+          "target": "7-playpark-entrance"
+        },
+        {
+          "yaw": -0.561513420400928,
+          "pitch": 0.0691266564419255,
+          "rotation": 0,
+          "target": "0-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bridge-1",
+      "name": "Bridge 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7331247526199256,
+          "pitch": 0.0864996690978348,
+          "rotation": 0,
+          "target": "4-playground-tree"
+        },
+        {
+          "yaw": -3.0710233053681133,
+          "pitch": 0.17969647748942208,
+          "rotation": 0,
+          "target": "7-playpark-entrance"
+        },
+        {
+          "yaw": -0.3956741347037003,
+          "pitch": 0.20760769554809855,
+          "rotation": 0,
+          "target": "6-bridge-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bridge-2",
+      "name": "Bridge 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.04652875192221,
+          "pitch": 0.1720413127036835,
+          "rotation": 0,
+          "target": "7-playpark-entrance"
+        },
+        {
+          "yaw": -1.7044846975530383,
+          "pitch": 0.09384088380629407,
+          "rotation": 0,
+          "target": "4-playground-tree"
+        },
+        {
+          "yaw": 3.1176570607536362,
+          "pitch": 0.3585616924660222,
+          "rotation": 0,
+          "target": "5-bridge-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-playpark-entrance",
+      "name": "PLaypark Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7772600612225773,
+          "pitch": 0.02099471862550928,
+          "rotation": 0,
+          "target": "6-bridge-2"
+        },
+        {
+          "yaw": 0.6790634694251576,
+          "pitch": -0.032079815708385695,
+          "rotation": 0,
+          "target": "6-bridge-2"
+        },
+        {
+          "yaw": -0.7120403188300646,
+          "pitch": 0.028981334597576236,
+          "rotation": 0,
+          "target": "3-restaurant"
+        },
+        {
+          "yaw": 0.18633573010142968,
+          "pitch": -0.10955722037164861,
+          "rotation": 0,
+          "target": "4-playground-tree"
+        },
+        {
+          "yaw": -1.497397144182946,
+          "pitch": 0.0690939034654221,
+          "rotation": 0,
+          "target": "2-mix-n-match"
+        },
+        {
+          "yaw": 2.9425035033441507,
+          "pitch": 0.10251856698100958,
+          "rotation": 0,
+          "target": "8-pots"
+        },
+        {
+          "yaw": -2.262853777699249,
+          "pitch": 0.12071554767785564,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": -2.026515062930205,
+          "pitch": 0.11480387131643965,
+          "rotation": 0,
+          "target": "1-entrance-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pots",
+      "name": "Pots",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4181564068317165,
+          "pitch": 0.14049603372800057,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 2.7386812724281793,
+          "pitch": 0.14634969740216697,
+          "rotation": 0,
+          "target": "1-entrance-2"
+        },
+        {
+          "yaw": -2.497861967091673,
+          "pitch": 0.10314864054668149,
+          "rotation": 0,
+          "target": "2-mix-n-match"
+        },
+        {
+          "yaw": -1.6790109607045132,
+          "pitch": 0.060416583947203506,
+          "rotation": 0,
+          "target": "3-restaurant"
+        },
+        {
+          "yaw": -0.7492843640782301,
+          "pitch": 0.00850592882282264,
+          "rotation": 0,
+          "target": "7-playpark-entrance"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
